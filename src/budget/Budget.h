9a090c364b3ffa31516fd1b696c5f5aeb6121b6f
Eric Bruhns <bruhe002@gmail.com>
2024-04-28 20:39:25 -0600

    Moving overloaded operators to cpp file

--- src/budget/Budget.h
+++ src/budget/Budget.h
@@ -9,9 +9,10 @@
 #define BUDGET_H_
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <fstream>
+
 #include <ctime>
 
 using namespace std;
@@ -26,17 +27,13 @@ struct Expense {
     float operator+(const Expense& exp) {
         return this->value_ + exp.value_;
     }
+
+    float operator+(float f) {
+        return f + value_;
+    }
 };
 
-// Overload the << operator
-ostream& operator<<(ostream& os, const Expense& exp) {
-    os << exp.name_ << "," << exp.value_ << "," << exp.fixed_ << "," << exp.income_ << "\n";
-    return os;
-}
 
-float operator+(float f, const Expense& e) {
-    return f + e.value_;
-}
 
 class Budget {
 public:
@@ -49,7 +46,7 @@ public:
     float sumUpExpenses();
     void setProfit(float value);
     float getProfit() const;
-    string displayBudget() const;
+    void displayBudget() const;
 
     ~Budget();
 
@@ -58,20 +55,7 @@ private:
      * Saves an Expense to a csv file once created, acting as our
      * database for saved budgets
     */
-    void saveExpenseToFile(const Expense& exp) {
-        // open file with username
-        string filename = username_ + "_expense_storage.csv";
-        ofstream file;
-        file.open(filename, ios::app);
-
-        // check if open
-        if(!file) {
-            // Handle errors later
-        } else {
-            file << exp;
-            file.close();  // Close file
-        }
-    }
+    void saveExpenseToFile(const Expense& exp);
 
     const string username_;
     float profit_;
