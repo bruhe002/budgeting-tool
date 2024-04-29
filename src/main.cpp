ef0685924206b2283e219046eec3f627300f8d9a
Eric Bruhns <bruhe002@gmail.com>
2024-04-28 18:19:34 -0600

    main file created

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////
+// @main.cpp
+//
+// @author Eric Bruhns
+//
+////////////////////////////////////////////////////////////
+
+#include "budget/Budget.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace budget;
+
+int main() {
+
+
+    return 0;
+}
