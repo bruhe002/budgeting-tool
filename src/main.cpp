a02ccbad0dddc8d1c1eb059fd7a8c072cd64da3f
Eric Bruhns <bruhe002@gmail.com>
2024-05-05 22:39:35 -0600

    Started UI in main file

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,53 @@
 #include "budget/Budget.h"
 
 #include <iostream>
+#include <exception>
 
 using namespace std;
 using namespace budget;
 
-int main() {
+bool hasSpace(const string& str);
 
+int main() {
+    string user = "";
+    string menu_choice = "";
+    while(menu_choice != "4") {
+        cout << "Welcome to Your Budget!\n\n";
+        cout << "Select the following options:\n";
+        cout << "\t1. Sign-in\n";
+        cout << "\t2. Create New User\n";
+        cout << "\t3. Delete a user\n";
+        cout << "\t4. Exit\n";
+        getline(cin, menu_choice);
+
+        if(menu_choice.size() != 1) {
+            cerr << "Invalid Option! Please try again...\n";
+        }
+        else {
+            if(menu_choice == "1") {
+
+            } else if(menu_choice == "2") {
+
+            } else if(menu_choice == "3") {
+
+            } else if(menu_choice == "4") {
+
+            } else {
+                cerr << "Invalid Option! Please try again...\n";
+            }
+        }
+        cout << "Please Enter your user name (Type \"-1\" to exit): ";
+        getline(cin, user);
+        try {
+            if(hasSpace(user)) {
+                throw;
+            }
+        } catch (exception& e) {
+            cerr << "User does not exist!\n" << "Creating one" 
+        }
+
+    }
+    
     Budget b;
     Expense rent{"Rent", 1885.00, FIXED_C};
     Expense car{"Car", 300.07, FIXED_C};
@@ -34,4 +75,14 @@ int main() {
     b.displayBudget();
 
     return 0;
+}
+
+bool hasSpace(const string& str) {
+    for(auto c : str) {
+        if(isspace(c)) {
+            return true;
+        }
+    }
+
+    return false;
 }
