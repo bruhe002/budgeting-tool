31624f3921ff1dd166eab9cb944f5770b13c49fa
Eric Bruhns <bruhe002@gmail.com>
2024-05-11 09:30:50 -0600

    Removed comments

--- src/main.cpp
+++ src/main.cpp
@@ -55,24 +55,6 @@ int main() {
             }
         }
     }
-
-    // Expense rent{"Rent", 1885.00, FIXED_C};
-    // Expense car{"Car", 300.07, FIXED_C};
-    // Expense car_in{"car_insurance", 134.21, FIXED_C};
-    // Expense income{"paycheck", 4710.00, FIXED_I};
-    // Expense food{"chick-fil-a", 25.04, ONE_TIME_C};
-    // Expense bday{"Birthday Money", 200.00, ONE_TIME_I};
-    // Expense netflix{"Netflix", 15.99, ONE_TIME_C};
-
-    // b.addExpense(rent);
-    // b.addExpense(car);
-    // b.addExpense(car_in);
-    // b.addExpense(income);
-    // b.addExpense(food);
-    // b.addExpense(bday);
-    // b.addExpense(netflix);
-
-
     return 0;
 }
 
